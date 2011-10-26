cae0f04734ea3c7e3ea1b1e8f3dfe02bfa31a84a
ramiro.rojo.cretta@gmail.com <ramiro.rojo.cretta@gmail.com@fa572930-dc10-f8e0-3d8e-a03e061a83cf>
2011-10-26 03:48:18 +0000

    Started Audio Library Added Audio File Template Types: (Midi, Mp3, Ogg, WMA, Wav)

---
+++ trunk/jGEL - Library/src/org/jgel/audio/files/Mp3File.java
@@ -0,0 +1,85 @@
+package org.jgel.audio.files;
+
+import org.jgel.audio.MusicFile;
+
+public class Mp3File implements MusicFile
+{
+
+	@Override
+	public void play()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fade(int time)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVolume(int volume)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPitch(int pitch)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPan(int pan)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getVolume()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPitch()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPan()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
