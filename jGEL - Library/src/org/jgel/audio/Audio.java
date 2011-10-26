3de76f324215c49fa5b3d6620ec5f0eae8de57e9
ramiro.rojo.cretta@gmail.com <ramiro.rojo.cretta@gmail.com@fa572930-dc10-f8e0-3d8e-a03e061a83cf>
2011-10-26 03:48:18 +0000

    Started Audio Library Added Audio File Template Types: (Midi, Mp3, Ogg, WMA, Wav)

---
+++ jGEL - Library/src/org/jgel/audio/Audio.java
@@ -0,0 +1,25 @@
+package org.jgel.audio;
+
+public abstract class Audio
+{
+	public static final void bgmPlay(String filename, int volume, int pitch, int pan)
+	{
+		
+	}
+	
+	public static final void bgsPlay(String filename, int volume, int pitch, int pan)
+	{
+		
+	}	
+	
+	public static final void mePlay(String filename, int volume, int pitch, int pan)
+	{
+		
+	}	
+	
+	public static final void sePlay(String filename, int volume, int pitch, int pan)
+	{
+		
+	}	
+	
+}
