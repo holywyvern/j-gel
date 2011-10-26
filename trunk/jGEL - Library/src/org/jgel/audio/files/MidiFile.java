f06d061c08c5428f20ca43bd7a19c75f5cc2213f
ramiro.rojo.cretta@gmail.com <ramiro.rojo.cretta@gmail.com@fa572930-dc10-f8e0-3d8e-a03e061a83cf>
2011-10-26 04:11:19 +0000

    Started MIDI engine

--- trunk/jGEL - Library/src/org/jgel/audio/files/MidiFile.java
+++ trunk/jGEL - Library/src/org/jgel/audio/files/MidiFile.java
@@ -1,34 +1,62 @@
 package org.jgel.audio.files;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.jgel.applet.GameApplet;
 import org.jgel.audio.MusicFile;
 import org.jgel.audio.SoundFile;
 
 public class MidiFile implements MusicFile, SoundFile
 {
-
+	private AudioClip clip;
+	
 	public MidiFile(String filename, int volume, int pitch, int pan)
 	{
 		
+		try
+		{
+			File f = new File(filename);
+			clip = Applet.newAudioClip(f.toURI().toURL());
+		} catch (Exception e)
+		{
+			try
+			{
+				clip = Applet.newAudioClip(new URL(GameApplet.getCodeBase2(), filename));
+			} catch (Exception e1)
+			{
+				try
+				{
+					clip = Applet.newAudioClip(new URL(filename));
+				} catch (MalformedURLException e2)
+				{
+					clip = null;
+				}
+			}
+		}
+		if (clip != null)
+		{
+		}
 	}
 	
 	@Override
 	public void play()
 	{
-		// TODO Auto-generated method stub
-
+		clip.play();
 	}
 
 	@Override
 	public void stop()
 	{
-		// TODO Auto-generated method stub
-
+		clip.stop();
 	}
 
 	@Override
 	public void pause()
 	{
-		// TODO Auto-generated method stub
 
 	}
 
@@ -86,6 +114,13 @@ public class MidiFile implements MusicFile, SoundFile
 	{
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public void loop()
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
